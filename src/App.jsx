e86e2088d138aee8cc73287e681756e18f934beb
Jeff <doylejeff98@gmail.com>
2023-11-09 01:58:05 -0500

    all test pass using react vitest

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+}
+
+
+
+
+{/*<h1>Our First Test</h1>;*/ }
+
+export default App;
+
+
